c260f2e09ca9455e06bc505de119ac935d80d0f7
Diptam <halderdiptam@gmail.com>
2018-05-02 08:15:16 -0700

    Add files via upload

---
+++ application/views/pages/about.php
@@ -0,0 +1,4 @@
+<h2><?= $title ?></h2>
+<br><br>
+<p>This a Blog version 1.0</p>
+<p>Developed by Diptam Halder</p>
---
+++ application/views/pages/home.php
@@ -0,0 +1,3 @@
+<h2><?= $title ?></h2>
+<br><br>
+<p>Welcome to the Deep thought Blog</p>
